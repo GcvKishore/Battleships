ba76557b9348278b899001e19f7f09588b5286ff
GcvKishore <88710476+GcvKishore@users.noreply.github.com>
2021-10-05 20:27:33 +0530

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -36,7 +36,7 @@ def makeModel(data):
     data["computer Board"] = addShips(data["computer Board"],data["num Of Ships"]) 
     data["temporary Ship"] = []
     data["numOfShips"] = 0
-    data["winner"] = "comp"
+    data["winner"] = None
     return 
 
 '''
